f9c43f072b978e9557e4345800219c87823f58c0
KevinLai02 <jihn.smith629@gmail.com>
2024-09-21 21:59:58 +0800

    commit

---
+++ src/types.ts
@@ -0,0 +1,47 @@
+export interface ServerToClientEvents {
+    joinRoom: (messageData:{
+        userName: string,
+        roomID: string
+    }) => void;
+    onReady: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+    start: (messageData:{
+        message: string,
+    }) => void;
+    mora: (messageData:{
+        userName: string,
+        roomID: string,
+        mora: string
+    }) => void;
+    userJoin: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+  }
+
+export interface ClientToServerEvents {
+    join: (roomId:string) => void;
+    joinRoom: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+    onReady: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+    start: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+    mora: (messageData:{
+        userName: string,
+        roomID: string,
+        mora: string
+    }) => void;
+    userJoin: (messageData:{
+        userName: string,
+        roomID: string,
+    }) => void;
+  }
